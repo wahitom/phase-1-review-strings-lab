e6c345f95a77b0a2c153d102813d7febbf77a086
Miriam Wahito Maina <miriammaina51@gmail.com>
2023-09-18 19:46:21 +0300

    Complete Assignment

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Write your code in this file!
+const currentUser = "Miriam Maina";
+const  welcomeMessage = "Welcome to Flatbook, " + currentUser + "!";
+const excitedWelcomeMessage = "WELCOME TO FLATBOOK, MIRIAM MAINA !";
+const   shortGreeting = "Welcome, M!";
